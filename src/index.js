a2751b5441109474c2722012b33c12a2fcb02de3
gilmarvogel <39626242+gilmarvogel@users.noreply.github.com>
2018-05-28 17:58:35 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+
+// a linha do redux devtools quer dizer que se ele viver no nosso objeto windows, ela pode ser invocada,
+// isso permite que se use a devtools do redux
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()   
+)
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
+
+
+
+//criar store e configurá-la para se igualar a invocação de chamar "createStore"
+//que vem do Redux e vai passar "createStore" para o reduce que foi criado   
